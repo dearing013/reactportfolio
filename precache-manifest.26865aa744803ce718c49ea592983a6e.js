8abe47e4cf9273216cd8c5b8762f136e72b9e0e1
Eric Dearing <ericdearing24@yahoo.com>
2020-02-07 23:43:38 -0500

    Updates

---
+++ precache-manifest.26865aa744803ce718c49ea592983a6e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e289230ef0dbf726362550b4daad8f79",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b4ed339bbdde389c73e",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "d68fac33dbb914d6fad7",
+    "url": "/static/js/2.d9dfd4c2.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.d9dfd4c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b4ed339bbdde389c73e",
+    "url": "/static/js/main.4b2d38f3.chunk.js"
+  },
+  {
+    "revision": "76ba2de154fd6435eca6",
+    "url": "/static/js/runtime-main.a20641ec.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
